23d8a39b6ff243d43f84130b4932f13a33b06ded
Carson Secrest <carsonsecrest@gmail.com>
2023-11-07 19:12:48 -0800

    Created logic to track when an ad is playing.

---
+++ game.js
@@ -0,0 +1,28 @@
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+console.log("IT'S WORKING!!!");
+
+let ad_shown = false;
+
+// Callback function executes with any change to the page's html (DOM)
+var observer = new MutationObserver(function(mutations){
+    const ad_contents = document.querySelector("div.ad-showing");
+    // this is the same as doing control + f and searching for "div.ad-showing"
+    if (ad_contents && !ad_shown) {
+        // ad is active as a video 
+        ad_shown = true;
+        console.log("Ad playing!!!")
+
+        }  
+    else if(!ad_contents && ad_shown) {
+        ad_shown = false;
+        // console.log("Video is playin!!!")
+    }
+    
+});
+
+// start observing DOM
+observer.observe(document, {
+    subtree: true,
+    attributes: true
+});
