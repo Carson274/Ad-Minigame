fd3b1dbc604c1342be521a1d943e156d77168acc
Carson Secrest <carsonsecrest@gmail.com>
2023-11-10 18:52:52 -0800

    Adding ad skipping.

--- game.js
+++ game.js
@@ -4,28 +4,63 @@ console.log("IT'S WORKING!!!");
 
 let ad_shown = false;
 
+let image;
+
 // Callback function executes with any change to the page's html (DOM)
 var observer = new MutationObserver(function(mutations){
     // this is the same as doing control + f and searching for "div.ad-showing"
-    const ad_contents = document.querySelector("div.ad-showing");
-    
-    if(ad_contents){
-        let contents = ad_contents.innerHTML;
+    const ad_container = document.querySelector("div.ad-showing");
+    const ad_video = document.querySelector("video");
+
+    const bounds = ad_video.getBoundingClientRect();
+
+    // check to see if ad is playing
+    if(ad_container){
+        let contents = ad_container.innerHTML;
     }
 
-    if (ad_contents && !ad_shown) {
+    // check to see if ad is playing; place image if it is
+    if (ad_container && !ad_shown) {
+
+        if(image){
+            // creates an image element with no_ad.png
+            image = document.createElement("img");
+            image.src = chrome.runtime.getURL("/images/no_ad.png");
+            image.id = "ad_image";
+            image.style.position = "absolute";
+            image.style.width = bounds.width + "px";
+            image.style.height = bounds.height + "px";
+            image.style.top = bounds.y + "px";
+            image.style.left = bounds.x + "px";
+            image.style.zIndex = "99";
+            console.log("Image displayed.");
+
+            document.body.appendChild(image);
+        }
+
         // ad is active as a video 
         ad_shown = true;
         console.log("Ad playing!!!");
 
-        ad_contents.innerHTML = `<img src='${chrome.runtime.getURL('/images/no_ad.png')}' style="width: 100%;">`;
-        console.log("We made it!");
+
+        setTimeout(() => {
+            const button = document.querySelector("button.ytp-ad-skip-button, button.ytp-ad-skip-button-modern");
+            console.log(button);
+            
+            if(button){
+                button.click();
+                console.log("Button attempted to click.");
+            }
+            console.log("Button attempted to click - after if.");
+        }, 5000);
     }  
-    else if(!ad_contents && ad_shown) {
+    else if(!ad_container && ad_shown) {
+
+        // remove the image from the page
+        document.querySelector("#ad_image").remove();
+
         ad_shown = false;
         console.log("Ad no longer playing.");
-
-        ad_contents.innerHTML = contents;
     }
     
 });
